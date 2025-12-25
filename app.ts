143d08a3e2e6de241c0b5c7d3dbd04a49706b703
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-12-25 11:15:28 +0100

    add cors + config it, also add routes and error handlers

--- app.ts
+++ app.ts
@@ -1,9 +1,28 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import userRoutes from "./routes/user";
+import errorRoute from "./routes/error";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+
+app.use(cookieParser());
+
+app.use("/api/user", userRoutes);
+
+app.use(errorRoute.notFoundError);
+
+app.use(errorRoute.errorRoute);
 
 export default app;
