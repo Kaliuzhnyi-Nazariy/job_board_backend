bcc3744c7a9d32b0e272c960b22d0f87b0575d27
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-09 19:31:57 +0100

    refactored user service and controller

--- controllers/user.ts
+++ controllers/user.ts
@@ -8,80 +8,68 @@ import helper from "../helper";
 const getMe = async (req: Request, res: Response, next: NextFunction) => {
   const { userId } = req as unknown as CustomRequest;
 
-if(!userId) {
-return next(errorHandler(401, "user not found"))
-}
+  if (!userId) {
+    return next(errorHandler(401, "User not found"));
+  }
 
+  try {
+    const user = await userService.getMe(userId);
 
-//  const result = await 
-//userService.getMe(userId);
-
-//  if (!result.ok) {
-//    return next(errorHandler(result.code, result.message));
-//  }
-//
-//  res.status(200).json(result);
-
-try {
-const user = await UserService.getMe(userId)
-
-return res.status(200).json(user)
-
-} catch (error) {
-next(error}
-
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const changePassword = async (req: Request, res: Response, next: NextFunction) => {
+const changePassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   const { oldPassword, newPassword, confirmPassword } = req.body;
   const { userId } = req as unknown as CustomRequest;
 
-if(!oldPassword) {
-return next(errorHandler(400, "Old password is not found"))
-}
+  if (!userId) {
+    return next(errorHandler(401, "User not found"));
+  }
 
-if(!newPassword) {
-return next(errorHandler(400, "New password is not found"))
-}
+  if (!oldPassword || !newPassword || !confirmPassword) {
+    return next(errorHandler(400, "Missing required fields"));
+  }
 
-if(!confirmPassword) {
-return next(errorHandler(400, "Confirm Password is not found"))
-}
+  if (newPassword !== confirmPassword) {
+    return next(errorHandler(400, "Invalid credentials"));
+  }
 
-if(!userId) {
-return next(errorHandler(401, "user is not found"))
-}
+  try {
+    await userService.changePassword(oldPassword, newPassword, userId);
 
-try {
-  await userService.changePassword(
-    oldPassword,
-    newPassword,
-    confirmPassword,
-    userId,
-  );
-
-  res.status(204).json();
-} catch (error){
-next(error)
-}
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const deleteAccount = async (req: Request, res: Response, next: NextFunction) => {
+const deleteAccount = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   const { userId } = req as unknown as CustomRequest;
 
-if(!userId) {
-return next(errorHandler(401, "user is not found"))
-}
+  if (!userId) {
+    return next(errorHandler(401, "User not found"));
+  }
 
-try {
-  await userService.deleteAccount(userId);
+  try {
+    await userService.deleteAccount(userId);
 
-  res.clearCookie("token", helper.cookieSettings);
+    res.clearCookie("token", helper.cookieSettings);
 
-  res.status(204).json();
-} catch (error) {
-next(error)
-}
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default { getMe, changePassword, deleteAccount };
--- sevice/user.ts
+++ sevice/user.ts
@@ -1,5 +1,6 @@
+import { errorHandler } from "../helper/errorHandler";
 import db from "../lib/db";
-import { GetMe, IUser } from "./interfaces";
+import { IUser } from "./interfaces";
 import bcrypt from "bcryptjs";
 
 /*
@@ -7,65 +8,56 @@ import bcrypt from "bcryptjs";
   return all data except for password
 */
 
-const getMe = async (userId: string): Promise<GetMe> => {
+const getMe = async (userId: string): Promise<IUser> => {
   const user = await db.query<IUser>(
     "SELECT role, username, full_name, email FROM users WHERE id=$1;",
     [userId],
   );
 
-  if (user.rows.length === 0) {
-    return { ok: false, code: 404, message: "User not found!" };
+  if (!user.rows[0]) {
+    throw errorHandler(404, "User not found");
   }
 
-  return { ok: true, user: { ...user.rows[0], id: userId } };
+  return { ...user.rows[0], id: userId };
 };
 
 const changePassword = async (
   oldPassword: string,
   newPassword: string,
-  confirmPassword: string,
   id: string,
 ) => {
-  try {
-    if (newPassword !== confirmPassword) {
-      throw Error("Passwords not match");
-    }
+  const userPassword = await db.query(
+    "SELECT password FROM users WHERE id = $1",
+    [id],
+  );
 
-    const userPassword = await db.query(
-      "SELECT password FROM users WHERE id = $1",
-      [id],
-    );
-
-    const isPasswordMatch = await bcrypt.compare(
-      oldPassword,
-      userPassword.rows[0].password,
-    );
-
-    if (!isPasswordMatch) {
-      throw Error("Old message is not match!");
-    }
-
-    if (oldPassword == confirmPassword) {
-      throw Error("New password cannot be the same as old one!");
-    }
-
-    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
-
-    await db.query("UPDATE users SET password = $1 WHERE id = $2", [
-      hashedNewPassword,
-      id,
-    ]);
-  } catch (error) {
-    throw error;
+  if (!userPassword.rows[0]) {
+    throw errorHandler(404, "User not found");
   }
+
+  const isPasswordMatch = await bcrypt.compare(
+    oldPassword,
+    userPassword.rows[0].password,
+  );
+
+  if (!isPasswordMatch) {
+    throw errorHandler(400, "Invalid credentials");
+  }
+
+  if (oldPassword == newPassword) {
+    throw errorHandler(400, "Invalid credentials");
+  }
+
+  const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+
+  await db.query("UPDATE users SET password = $1 WHERE id = $2", [
+    hashedNewPassword,
+    id,
+  ]);
 };
 
 const deleteAccount = async (id: string) => {
-  try {
-    await db.query("DELETE FROM users WHERE id=$1", [id]);
-  } catch (error) {
-    throw error;
-  }
+  await db.query("DELETE FROM users WHERE id=$1", [id]);
 };
 
 export default { getMe, changePassword, deleteAccount };
