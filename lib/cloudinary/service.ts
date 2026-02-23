c36f6e1b2ef3d87c6a82c73a6627d717f2e1f8c6
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-02-23 16:18:41 +0100

    add cloudinry

---
+++ lib/cloudinary/service.ts
@@ -0,0 +1,20 @@
+import cloudinary from "./config";
+
+export const uploadPhoto = async (
+  file: Express.Multer.File,
+  userId?: string,
+) => {
+  // console.log(file);
+
+  const ret = await cloudinary.uploader.upload(file.path, {
+    folder: "job_board_account_images",
+    public_id: `user_${userId}`,
+    overwrite: true,
+  });
+
+  return ret;
+};
+
+export const deleteCloudPhoto = async (userId: string) => {
+  await cloudinary.uploader.destroy(`user_${userId}`);
+};
