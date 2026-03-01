2d11bb6e318ff1917a8175afe54cbc6ab1bc136f
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-03-01 15:04:48 +0100

    chnage naming and add data for external databse

--- lib/db.ts
+++ lib/db.ts
@@ -3,14 +3,14 @@ import { Pool } from "pg";
 let db: Pool;
 
 if (process.env.NODE_ENV === "production") {
-  const { PG_HOST, PG_PORT, PG_PASSWORD, PG_USER, PG_DATABASE } = process.env;
+  const { DB_HOST, DB_PORT, DB_PASSWORD, DB_USER, DB_DATABASE } = process.env;
 
   db = new Pool({
-    host: PG_HOST,
-    user: PG_USER,
-    password: PG_PASSWORD,
-    database: PG_DATABASE,
-    port: Number(PG_PORT),
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    port: Number(DB_PORT),
   });
 } else {
   const { PG_HOST, PG_PORT, PG_PASSWORD, PG_USER, PG_DATABASE } = process.env;
