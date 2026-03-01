a7d64abb283732a748ab03f582d28f926802503c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-03-01 19:07:20 +0100

    add frontend to cors origin

--- app.ts
+++ app.ts
@@ -15,7 +15,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://job-board-eight-beta.vercel.app/",
+    ],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   }),
