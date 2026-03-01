c499e8d719ce342963b32ef573b0825655a7ea49
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-03-01 19:46:46 +0100

    add link to cors

--- app.ts
+++ app.ts
@@ -18,6 +18,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://job-board-eight-beta.vercel.app/",
+      "https://job-board-eight-beta.vercel.app",
     ],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
