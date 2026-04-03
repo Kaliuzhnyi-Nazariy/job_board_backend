11e8a00b55914d500549f5e83f1aa754d828b675
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-04-03 16:41:04 +0200

    fix: changed cookie settings

--- helper/cookieSettings.ts
+++ helper/cookieSettings.ts
@@ -1,9 +1,20 @@
-export const cookieSettings = {
+// export const cookieSettings = {
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === "production",
+//   maxAge: 3 * 24 * 60 * 60 * 1000,
+//   sameSite: "none" as "none",
+//   // sameSite: (process.env.NODE_ENV === "production" ? "none" : "lax") as
+//   //   | "none"
+//   // | "lax",
+//   path: "/",
+// };
+
+import { CookieOptions } from "express";
+
+export const cookieSettings: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  maxAge: 3 * 24 * 60 * 60 * 1000,
-  sameSite: (process.env.NODE_ENV === "production" ? "none" : "lax") as
-    | "none"
-    | "lax",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   path: "/",
+  maxAge: 86400000,
 };
