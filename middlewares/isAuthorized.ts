e941c9dec70589f2044fd30f1be7910087947abc
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-07-08 17:49:12 +0200

    fix: add interface and make optional

--- middlewares/isAuthorized.ts
+++ middlewares/isAuthorized.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Response, Request } from "express";
 import { errorHandler } from "../helper/errorHandler";
+import { CustomRequest } from "./interfaces";
 
 export type Roles = "candidate" | "employer";
 
 export const isAuthorized =
   (allowedRoles: Roles[]) =>
-  (req: Request, res: Response, next: NextFunction) => {
+  (req: CustomRequest, res: Response, next: NextFunction) => {
     if (!req.role) {
       return next(errorHandler(401));
     }
